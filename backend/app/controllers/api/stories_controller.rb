c01f68b59ed3a67b1b821ee21b1b1bdefd4c3b9d
elpopes <lorenzotijerina@gmail.com>
2024-10-28 11:58:10 -0400

    add S3 functionality

--- backend/app/controllers/api/stories_controller.rb
+++ backend/app/controllers/api/stories_controller.rb
@@ -1,9 +1,38 @@
-module Api
-    class StoriesController < ApplicationController
-        def index
-            @stories = Story.all
-            render json: @stories
+class Api::StoriesController < ApplicationController
+    def create
+        @story = Story.new(story_params)
+        @story.image.attach(params[:image]) if params[:image].present? # Attach image
+      
+        if @story.save
+            render json: @story, status: :created
+        else
+            render json: @story.errors, status: :unprocessable_entity
         end
     end
+
+    def index
+        @stories = Story.all
+        render json: @stories.map { |story| story_json(story) }
+    end
+      
+      private
+      
+    def story_json(story)
+        {
+            id: story.id,
+            title: story.title,
+            content: story.content,
+            language: story.language,
+            image_url: story.image.attached? ? url_for(story.image) : nil # Include the image URL
+        }
+    end
+      
+  
+    private
+  
+    def story_params
+        params.require(:story).permit(:title, :content, :language, :image) # Permit image parameter
+    end
 end
+  
   
