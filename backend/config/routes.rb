939cbd94b74199d7733c38807a0c33c72a2ddd61
elpopes <lorenzotijerina@gmail.com>
2024-11-20 10:45:11 -0500

    update routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
     namespace :api do
         resources :stories, only: [:index, :show, :create] 
+        resources :stories, only: [:index, :show, :create]
+        resources :authors, only: [:index, :show, :create]
         get 'transit', to: 'transit#get_transit_info'
         post 'signup', to: 'auth#signup'
         post 'login', to: 'auth#login'
