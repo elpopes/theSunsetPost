becffe523f208682f5bdae52e3df162031c2cfb5
elpopes <lorenzotijerina@gmail.com>
2025-05-12 10:11:00 -0400

    update language switcher to adjust url

--- frontend/src/components/LanguageSwitcher.js
+++ frontend/src/components/LanguageSwitcher.js
@@ -1,18 +1,27 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import "./LanguageSwitcher.css"; // Import the CSS file
+import { useLocation, useNavigate } from "react-router-dom";
+import "./LanguageSwitcher.css";
 
 const LanguageSwitcher = () => {
   const { i18n } = useTranslation();
+  const location = useLocation();
+  const navigate = useNavigate();
 
-  const changeLanguage = (lang) => {
-    i18n.changeLanguage(lang);
+  const changeLanguage = (lng) => {
+    if (i18n.language === lng) return; // avoid reprocessing if already selected
+
+    i18n.changeLanguage(lng);
+    const pathWithoutLang = location.pathname.replace(/^\/(en|es|zh)/, "");
+    const newPath = `/${lng}${pathWithoutLang || "/"}`;
+
+    navigate(newPath, { replace: true }); // React-router navigation (no reload)
   };
 
   return (
     <div className="language-switcher">
-      <button onClick={() => changeLanguage("en")}>English</button>
-      <button onClick={() => changeLanguage("es")}>Español</button>
+      <button onClick={() => changeLanguage("en")}>EN</button>
+      <button onClick={() => changeLanguage("es")}>ES</button>
       <button onClick={() => changeLanguage("zh")}>中文</button>
     </div>
   );
