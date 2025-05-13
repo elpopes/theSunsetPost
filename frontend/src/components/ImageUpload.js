0f3820e6045fd2e690e7e411791051b561a7534d
elpopes <lorenzotijerina@gmail.com>
2025-05-13 15:27:46 -0400

    add image preview for featured image

--- frontend/src/components/ImageUpload.js
+++ frontend/src/components/ImageUpload.js
@@ -1,10 +1,27 @@
 import React from "react";
 
-const ImageUpload = ({ image, setImage }) => (
-  <div>
-    <label>Featured Image (used for social sharing):</label>
-    <input type="file" onChange={(e) => setImage(e.target.files[0])} />
-  </div>
-);
+const ImageUpload = ({ image, setImage }) => {
+  const handleChange = (e) => {
+    const file = e.target.files[0];
+    if (file) setImage(file);
+  };
+
+  return (
+    <div>
+      <label>Featured Image (used for social sharing):</label>
+      <input type="file" accept="image/*" onChange={handleChange} />
+      {image && (
+        <div style={{ marginTop: "10px" }}>
+          <p>Preview:</p>
+          <img
+            src={URL.createObjectURL(image)}
+            alt="Preview"
+            style={{ maxWidth: "100%", maxHeight: "300px" }}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default ImageUpload;
